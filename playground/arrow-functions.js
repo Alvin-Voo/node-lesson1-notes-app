bc87ccb6bd870a99307c2aa43aa5a9c214fdb186
Alvin Voo <alvin.voo@gmail.com>
2018-05-06 16:28:44 +0800

    adding some comments

--- playground/arrow-functions.js
+++ playground/arrow-functions.js
@@ -8,10 +8,15 @@ let user ={
   sayHi: () =>{
     console.log(arguments);//this is some generic 'global' value
     console.log(`Hi. I'm ${this.name}`);//this is not binded here
+    //- in short, anything that you need to access in the same block scope that need to be
+    //referenced by the this keyword is not accessible inside an arrow function
+    console.log(name);//global variable is accessible
   },
   sayHi2 () {//this is same as the one below
     console.log(arguments);//this will pass the arguments object
     console.log(`Hi. I'm ${this.name}`);
+    console.log(name);//global variable is accessible
+
   },
   sayHi3: function(){
     console.log(`Hi. I'm ${this.name}`);
